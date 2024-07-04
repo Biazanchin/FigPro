d86522d45d8f1360ff7e0e7836a7a9c98f75bed5
Biazanchin <beatrizzanquimmessias@gmail.com>
2024-07-03 21:45:11 -0300

    initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,32 @@
+import logo from "../../assets/logo.svg";
+import search from "../../assets/search-2905.svg";
+import user from "../../assets/sign-out-3300.svg";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <img src={logo} alt="Logo" />
+        <nav>
+          <a href="#" className="active">
+            HOME
+          </a>
+          <a href="#">ABOUT</a>
+          <a href="#">EXAMPLES</a>
+          <a href="#">CONTACT</a>
+        </nav>
+        <div className="right">
+          <button type="button">
+            <img src={search} alt="search icon" />
+          </button>
+          <button type="button">
+            <img src={user} alt="user icon" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/heroHome/HeroHome.jsx
@@ -0,0 +1,6 @@
+import "./HeroHome.css";
+const HeroHome = () => {
+  return <div>HeroHome</div>;
+};
+
+export default HeroHome;
