261b3743429509f74e0d1ad2d88fa14ae1269bdf
Biazanchin <beatrizzanquimmessias@gmail.com>
2024-07-03 23:43:58 -0300

    feat: add HeroHome

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/header/Header";
+import HeroHome from "../components/heroHome/HeroHome";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <HeroHome />
     </div>
   );
 };
