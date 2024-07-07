20ac41742cba3c9d63128945b9ec5b5746764ee0
Biazanchin <beatrizzanquimmessias@gmail.com>
2024-07-07 19:01:58 -0300

    feat: add info section

---
+++ src/components/info/Info.jsx
@@ -0,0 +1,38 @@
+import owl from "../../assets/owl1.png";
+import "./Info.css";
+
+const Info = () => {
+  return (
+    <section className="s-info">
+      <div className="container">
+        <p>
+          Discover stickers that are true stars in our catalog! Our Featured
+          Section showcases the most incredible and enchanting creations, ready
+          to bring life to your chats and make your conversations even more
+          special.
+        </p>
+        <div className="box">
+          <img src={owl} alt="owl sticker" className="ilustra" />
+          <div className="content">
+            <ul className="text">
+              <li>
+                <strong>sticker library</strong>
+                <h3>+1000</h3>
+              </li>
+              <li>
+                <strong>free stickers</strong>
+                <h3>+293</h3>
+              </li>
+              <li>
+                <strong>stickers created every day</strong>
+                <h3>+500</h3>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Info;
--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/header/Header";
 import HeroHome from "../components/heroHome/HeroHome";
+import Info from "../components/info/Info";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <HeroHome />
+      <Info />
     </div>
   );
 };
