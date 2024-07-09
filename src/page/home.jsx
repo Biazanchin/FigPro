92baf98833e50db0e774291146c1cab34171926c
Biazanchin <beatrizzanquimmessias@gmail.com>
2024-07-09 02:32:00 -0300

    feat: add new component

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/header/Header";
 import HeroHome from "../components/heroHome/HeroHome";
 import Info from "../components/info/Info";
+import Steps from "../components/steps/Steps";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Header />
       <HeroHome />
       <Info />
+      <Steps />
     </div>
   );
 };
